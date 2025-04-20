66e8eda674335a897d50553a250529b2f37f119b
Zongshuo Han <zongshuohan009@gmail.com>
2025-04-20 12:38:11 +0300

    add config.ini file

--- user_service/database.py
+++ user_service/database.py
@@ -1,10 +1,19 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import configparser
 
-DATABASE_URL = "postgresql://postgres:123456@localhost:5432/Distributed_Project"
+# 读取配置文件
+config = configparser.ConfigParser()
+config.read('config.ini')
+db_config = config['postgresql']
 
-engine = create_engine(DATABASE_URL)
+# 构建数据库URL（SQLAlchemy格式）
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
+
+# 创建SQLAlchemy引擎和会话
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# 声明基类
 Base = declarative_base()
